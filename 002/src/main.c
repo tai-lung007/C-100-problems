6c96f5b499cfa71c8e6e727fc9c31ea545cc632d
tailung <tailungsrage@gmail.com>
2024-10-12 11:07:27 +0530

    display n and n-1Wq

--- 002/src/main.c
+++ 002/src/main.c
@@ -1,8 +1,44 @@
 #include<stdio.h>
-
+void displayBits(int num);
 int main()
 {
-printf("hello world\n");
+	int i;
+	for(i=23 ;i<=39; i++)
+	{
+	 printf("%d--->",i);
+	 displayBits(i);
+	 printf("      ");
+	 displayBits(i-1);
+
+	 printf("\n");
+	}
+
+
+
+
 
 return 0;
+}
+
+
+void displayBits(int num) { 
+	
+	int i;
+for(i=31;i>=0;i--)
+{
+	printf("%d",(num>>i)&1);
+	
+
+	if(i%4==0)
+		putchar(' ');
+
+
+
+
+}
+
+
+	printf("\n");
+
+
 }
